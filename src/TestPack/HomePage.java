fa93569d4ebe3e8047fe1101cc4921163b6b787f
Rajesh <kvm.rajesh@gmail.com>
2023-02-14 12:40:06 +0530

    2 files updated. Committing from master branch

--- src/TestPack/HomePage.java
+++ src/TestPack/HomePage.java
@@ -1,5 +1,9 @@
 package TestPack;
 
 public class HomePage {
+	public static void main(String[] args) {
+		System.out.println("This page handles Home page automation");
+	}
+	
 
 }
--- src/TestPack/LoginPage.java
+++ src/TestPack/LoginPage.java
@@ -2,4 +2,7 @@ package TestPack;
 
 public class LoginPage {
 
+	public static void main(String[] args) {
+		System.out.println("This is class handles login page autoamation");
+	}
 }
